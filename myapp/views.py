f0d53c4788dbd117e94472317238d02eec34c337
nagauzzi <kensaku.nagauchi@access-company.com>
2021-05-22 22:23:19 +0900

    Render

--- myapp/views.py
+++ myapp/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    return render(request, "hello/index.html")
 
 def greet(request, name):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+    return render(request, "hello/greet.html", {
+        "name": name.capitalize()
+    })
